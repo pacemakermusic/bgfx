0cda6b20098c4de70bb30f569825b5cd9087b67e
Bob Builder <dev+apple@pacemaker.net>
2018-09-07 15:08:34 +0200

    Always warn and check in debug

--- src/bgfx_p.h
+++ src/bgfx_p.h
@@ -17,6 +17,9 @@
 #	define BX_WARN  _BX_WARN
 #	define BX_CHECK _BX_CHECK
 #	define BX_CONFIG_ALLOCATOR_DEBUG 1
+#elif !defined(NDEBUG)
+#	define BX_WARN  _BX_WARN
+#	define BX_CHECK _BX_CHECK
 #endif // BGFX_CONFIG_DEBUG
 
 #include <bgfx/bgfx.h>
@@ -102,7 +105,7 @@ namespace bgfx
 				BX_MACRO_BLOCK_BEGIN \
 					if (!BX_IGNORE_C4127(_condition) ) \
 					{ \
-						BX_TRACE("WARN " _format, ##__VA_ARGS__); \
+						_BX_TRACE("WARN " _format, ##__VA_ARGS__); \
 					} \
 				BX_MACRO_BLOCK_END
 
@@ -110,7 +113,7 @@ namespace bgfx
 				BX_MACRO_BLOCK_BEGIN \
 					if (!BX_IGNORE_C4127(_condition) ) \
 					{ \
-						BX_TRACE("CHECK " _format, ##__VA_ARGS__); \
+						_BX_TRACE("CHECK " _format, ##__VA_ARGS__); \
 						bgfx::fatal(bgfx::Fatal::DebugCheck, _format, ##__VA_ARGS__); \
 					} \
 				BX_MACRO_BLOCK_END
